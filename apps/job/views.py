598b84dda09a3a270e5f452d1bfb53bc19d8708f
Cedric Baaklini <cabaaklini@icloud.com>
2024-06-23 17:09:45 -0400

    Adding notifications.

--- apps/job/views.py
+++ apps/job/views.py
@@ -2,12 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import AddJobForm, ApplicationForm
 from .models import Job
+from apps.notification.utillities import create_notification
 
 
 def job_detail(request, job_id):
     job = Job.objects.get(pk = job_id)
 
-    return render(request, 'job/job_detail.html', { 'job': job })
+    return render(request, 'job/job_detail.html', {'job': job})
 
 
 @login_required
@@ -22,6 +23,8 @@ def apply_for_job(request, job_id):
             application.job = job
             application.created_by = request.user
             application.save()
+
+            create_notification(request, job.created_by, 'application', extra_id = application.id)
 
             return redirect('dashboard')
         else:
@@ -44,4 +47,4 @@ def add_job(request):
     else:
         form = AddJobForm()
 
-    return render(request, 'job/add_job.html', { 'form': form })
+    return render(request, 'job/add_job.html', {'form': form})
---
+++ apps/notification/context_processors.py
@@ -0,0 +1,8 @@
+from apps.notification.models import Notification
+
+
+def notifications(request):
+    if request.user.is_authenticated:
+        return {'notifications': request.user.notifications.filter(is_read = False)}
+    else:
+        return {'notifications': []}
